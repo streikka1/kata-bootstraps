0d5076a621147379742c88195743a07695bf566c
Holger Schmeisky <google@schmeisky.com>
2018-01-21 21:56:26 +0100

    our kata

--- js/jasmine/spec/HelloWorldSpec.js
+++ js/jasmine/spec/HelloWorldSpec.js
@@ -1,5 +1,21 @@
 describe("Hello world", function() {
-  it("says hello", function() {
-    expect(helloWorld()).toEqual("Hello!");
+  it ("returns 1 to 100", function() {
+    expect(fizzbuzz()
+        .split(/\r\n|\r|\n/).length).toEqual(100);
+  });
+  it ("returns Fizz if divisible by 3 ", () => {
+    expect(convertFizzBuzz(3)).toEqual("Fizz");  
+  });
+  it ("returns Fizz if divisible by 3 ", () => {
+    expect(convertFizzBuzz(9)).toEqual("Fizz");  
+  });
+  it ("returns Buzz if divisible by 5 ", () => {
+    expect(convertFizzBuzz(10)).toEqual("Buzz");  
+  });
+  it ("returns Buzz if divisible by 5 ", () => {
+    expect(convertFizzBuzz(20)).toEqual("Buzz");  
+  });
+  it ("returns FizzBuzz if divisible by 3 and 5 ", () => {
+    expect(convertFizzBuzz(30)).toEqual("FizzBuzz");  
   });
 });
--- js/jasmine/src/HelloWorld.js
+++ js/jasmine/src/HelloWorld.js
@@ -1,4 +1,24 @@
-function helloWorld() {
-  return "Hello world!";
+function fizzbuzz() {
+    let s = "";
+    for (i = 1; i < 100; i++) { 
+        s = s + convertFizzBuzz(i) + "\n"
+    }
+    return s;
 }
 
+function convertFizzBuzz(number) {
+
+    let convertResult = "";
+    if (number % 3 == 0) {
+        convertResult += "Fizz";
+    }
+    if (number % 5 == 0) {
+        convertResult += "Buzz";
+    }
+    if (convertResult.length == 0) {
+        convertResult = number;
+    } 
+    return String(convertResult);
+}
+
+alert(fizzbuzz());
