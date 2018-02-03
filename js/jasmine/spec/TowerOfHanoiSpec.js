124fd61eb2fb25170dd622de4018dcb45e4cf31f
Thomas Johanndeiter <streikka1@gmail.com>
2018-02-03 16:55:10 +0100

    Add ToH object. Remove global vars from spec. Add recursion test (biggest disk never on smaller disk)

--- js/jasmine/spec/TowerOfHanoiSpec.js
+++ js/jasmine/spec/TowerOfHanoiSpec.js
@@ -1,49 +1,68 @@
 describe('Tower of Hanoi', () => {
-    const towerSourceName = "sourcePeg";
-    const towerTargetName = "auxPeg";
-    const towerAuxName = "targetPeg";
+    'use strict';
+
     const towerSize = 5;
 
     // Initial situation  
     // All disks are on source peg, sorted by size. 
     // Disk 1 is the smallest disk, i.e. at the end of a tower array  
-    let towerSource = [];
-    let towerTarget = [];
-    let towerAux = [];
-    let biggestDisk = towerSize
 
     beforeEach(() => {
-        for (let index = towerSize; index >= 1; index--) {
-            towerSource.push(index);
-        }
-        towerTarget = [];
-        towerAux = [];
-        // Create test object to track recursive function calls via spies 
-        spyOn(towerOfHanoi, 'move').and.callThrough();
+        this.myTower = new TowerOfHanoi(towerSize);
     });
 
     afterEach(() => {
-        towerSource = [];
+        this.myTower.reset();
     });
 
     it(`starts with ${towerSize} disks on source tower`, () => {
-        expect(towerSource.length).toBe(towerSize);
+        //let myTower = new TowerOfHanoi(towerSize); --> siehe before
+        expect(this.myTower.source.length).toBe(towerSize);
     });
 
-    it(`moves biggest disk ${biggestDisk} to target tower as bottom disk`, () => {
-        towerOfHanoi.move(towerSize, towerSource, towerTarget, towerAux);
-        expect(towerTarget.indexOf(biggestDisk)).toBe(0);
+    it(`moves biggest disk ${towerSize} to target tower as bottom disk`, () => {
+        //let myTower = new TowerOfHanoi(towerSize);
+        towerFuncs.move(towerSize, this.myTower.source, this.myTower.target, this.myTower.aux);
+        expect(this.myTower.target.indexOf(towerSize)).toBe(0);
     });
 
     it(`moves all disks from source to target, keeping the correct oder (smallest at the top)`, () => {
-        let towerSourceTmp = towerSource.slice();
-        expect(towerOfHanoi.move(towerSize, towerSource, towerTarget, towerAux)).toEqual(towerSourceTmp);
+        //let myTower = new TowerOfHanoi(towerSize);
+        let towerSourceTmp = myTower.source.slice();
+        expect(towerFuncs.move(towerSize, this.myTower.source, this.myTower.target, this.myTower.aux)).toEqual(towerSourceTmp);
+        expect(this.myTower.aux.length).toEqual(0);
     });
 
-    it('moves disks n-1..1 to auxPeg via recusion', () => {
-        // Use spy on first recursive call
-        towerOfHanoi.move(towerSize, towerSource, towerTarget, towerAux);
-        expect(towerOfHanoi.move.calls.argsFor(1)).toEqual([towerSize - 1, towerSource, towerAux, towerTarget]);
-    });
+    describe('ToH Recursion', () => {
 
+        it('moves disks n-1..1 to auxPeg via recusion', () => {
+            //let myTower = new TowerOfHanoi(towerSize);
+            // Use spy on first recursive call
+            spyOn(towerFuncs, 'move').and.callThrough();
+            towerFuncs.move(towerSize, this.myTower.source, this.myTower.target, this.myTower.aux);
+            expect(towerFuncs.move.calls.argsFor(1)).toEqual([towerSize - 1, this.myTower.source, this.myTower.aux, this.myTower.target]);
+
+        });
+
+        it('never places a bigger disk on a smaller disk', () => {
+            spyOn(towerFuncs, 'move').and.callThrough();
+            towerFuncs.move(towerSize, this.myTower.source, this.myTower.target, this.myTower.aux);
+            let sortingOk = true;
+            // label 
+           outerLoop:
+            for (let index = 0; index < towerFuncs.move.calls.count(); index++) {
+                //let argsOfRecursiveCall = towerFuncs.move.calls.argsFor(index);
+                towerFuncs.move.calls.argsFor(index).forEach(element => {
+                    if (Array.isArray(element) && element.length > 1) {
+                        // Biggest disk should be at first position
+                        if (element[0] < element[element.length - 1]) {
+                            sortingOk = false;
+                        }
+                    }
+                });
+            }
+            //One assertion per test case!
+            expect(sortingOk).toBe(true);
+        });
+    });
 });
--- js/jasmine/src/TowerOfHanoi.js
+++ js/jasmine/src/TowerOfHanoi.js
@@ -1,4 +1,26 @@
-let towerOfHanoi = {
+'use strict';
+
+// Constructor pattern  
+function TowerOfHanoi(size) {
+
+    // Init source peg
+    // biggest disk is on the left (i.e. first position in array)
+    this.source = [];
+    for (let index = size; index >= 1; index--) {
+        this.source.push(index);
+    }
+    this.target = [];
+    this.aux = [];
+
+    this.reset = () => {
+        this.source = [];
+        this.target = [];
+        this.aux = [];
+    }
+}
+
+// Function created within object to be able to spy on
+let towerFuncs = {
     move(size, sourcePeg = [], targetPeg = [], auxPeg = []) {
         if (size > 0) {
             this.move(size - 1, sourcePeg, auxPeg, targetPeg);
@@ -8,4 +30,3 @@ let towerOfHanoi = {
         }
     }
 }
-//towerOfHanoi.move(2, [2, 1]);
